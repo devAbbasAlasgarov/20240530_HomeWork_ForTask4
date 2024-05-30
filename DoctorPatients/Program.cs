d0167d895fd49198e4a22650bdfd81dff221e209
Abbas Alasgarov <devabbasalasgarov@gmail.com>
2024-05-30 07:43:50 +0400

    Add project files.

---
+++ DoctorPatients/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace DoctorPatients
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int numberOfPills = 0;
+            int sumOfPills = 0;
+            Console.WriteLine("Xəstə sayını daxil edin: ");
+            int numberOfPatients = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < numberOfPatients; i++)
+            {
+                numberOfPills = int.Parse(Console.ReadLine());
+                sumOfPills += numberOfPills;
+            }
+            Console.WriteLine($"Qəbul edilən xəstə sayı: {numberOfPatients}");
+            Console.WriteLine($"Xəstələrə yazlılan toplam dərman sayı: {sumOfPills}");
+            Console.WriteLine($"Xəstə başına düşən ortalama dərman sayı?: {sumOfPills/numberOfPatients}");
+        }
+    }
+}

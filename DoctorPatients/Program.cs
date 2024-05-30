20bb871b2f730bd6c2ff3e2d1e862eed8c0ba723
Abbas Alasgarov <devabbasalasgarov@gmail.com>
2024-05-30 08:24:07 +0400

    Applying SRP

--- DoctorPatients/Program.cs
+++ DoctorPatients/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Transactions;
 
 namespace DoctorPatients
 {
@@ -7,12 +8,23 @@ namespace DoctorPatients
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            int numberOfPills = 0;
-            int sumOfPills = 0;
+            int numberOfPatients = EnterNumberOfPatients();
+            EnterNumberOfPills(numberOfPatients);
+         
+        }
+
+        public static int EnterNumberOfPatients()
+        {
             Console.WriteLine("Xəstə sayını daxil edin: ");
             int numberOfPatients = int.Parse(Console.ReadLine());
+            return numberOfPatients;
+        }
 
-            for(int i = 1; i <= numberOfPatients; i++)
+        public static void EnterNumberOfPills(int numberOfPatients)
+        {
+            int numberOfPills = 0;
+            int sumOfPills = 0;
+            for (int i = 1; i <= numberOfPatients; i++)
             {
                 Console.Write($"Enter number of pills given to patient {i} : ");
                 numberOfPills = int.Parse(Console.ReadLine());
@@ -20,8 +32,12 @@ namespace DoctorPatients
             }
             Console.WriteLine($"Qəbul edilən xəstə sayı: {numberOfPatients}");
             Console.WriteLine($"Xəstələrə yazlılan toplam dərman sayı: {sumOfPills}");
-            Console.WriteLine($"Xəstə başına düşən ortalama dərman sayı: {sumOfPills/numberOfPatients}");
+            Console.WriteLine($"Xəstə başına düşən ortalama dərman sayı: {sumOfPills / numberOfPatients}");
             Console.Read();
         }
     }
+
+    
+
+
 }

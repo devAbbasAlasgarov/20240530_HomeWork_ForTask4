beddf7a8bfa6edada93f973f8ad9d981088c4585
Abbas Alasgarov <devabbasalasgarov@gmail.com>
2024-05-30 07:48:02 +0400

    Fixing number of pills

--- DoctorPatients/Program.cs
+++ DoctorPatients/Program.cs
@@ -12,7 +12,7 @@ namespace DoctorPatients
             Console.WriteLine("Xəstə sayını daxil edin: ");
             int numberOfPatients = int.Parse(Console.ReadLine());
 
-            for(int i = 0; i < numberOfPatients; i++)
+            for(int i = 1; i <= numberOfPatients; i++)
             {
                 Console.Write($"Enter number of pills given to patient {i} : ");
                 numberOfPills = int.Parse(Console.ReadLine());
@@ -20,7 +20,7 @@ namespace DoctorPatients
             }
             Console.WriteLine($"Qəbul edilən xəstə sayı: {numberOfPatients}");
             Console.WriteLine($"Xəstələrə yazlılan toplam dərman sayı: {sumOfPills}");
-            Console.WriteLine($"Xəstə başına düşən ortalama dərman sayı?: {sumOfPills/numberOfPatients}");
+            Console.WriteLine($"Xəstə başına düşən ortalama dərman sayı: {sumOfPills/numberOfPatients}");
         }
     }
 }

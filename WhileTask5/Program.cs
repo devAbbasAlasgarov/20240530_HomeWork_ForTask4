646afdb73f23886a8757065ed75ad5be8b708fdc
Abbas Alasgarov <devabbasalasgarov@gmail.com>
2024-06-01 19:28:06 +0400

    Initialization of variables

---
+++ WhileTask5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace WhileTask5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string resultOfBloodTest = "normal";
+            decimal priceOfDoctor = 90;
+            string professionOfDoctor;
+            string nameOfDoctor;
+            decimal positiveResult;
+            decimal negativeResult;
+
+            Console.WriteLine(5);
+
+        }
+
+        
+    }
+}
